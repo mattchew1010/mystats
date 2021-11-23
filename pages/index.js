2abde1731d07ced173412a13c39aa5e612310ab9
mattchew1010 <55364807+mattchew1010@users.noreply.github.com>
2021-11-23 12:33:06 -0500

    im the literal best

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home() {
       <>
          <title>Home</title>
          <Image 
-         src='/Home/nick.jpg'
+         src='/Home/cert.jpg'
          alt="Picture of a cute mf"
          width="500"
          height="600"
          />
 
-         <p>Nick kinda cute</p>
+         <p>#1 best guy</p>
       </>
    )
 }
