59995d8430b3f753510bfa799ba672b7903c3e37
mattchew1010 <55364807+mattchew1010@users.noreply.github.com>
2021-11-23 12:35:07 -0500

    change .jpg to .png

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
       <>
          <title>Home</title>
          <Image 
-         src='/Home/cert.jpg'
+         src='/Home/cert.png'
          alt="Picture of a cute mf"
          width="500"
          height="600"
