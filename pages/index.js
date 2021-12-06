f71cabaa026ffa87634eaed184833c398d4be8dc
mattchew1010 <mattchewiscool@gmail.com>
2021-12-06 03:45:09 -0500

    delete later

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,17 @@ export default function Login() {
       <>
          <Navbar/>
          <title>Home</title>
+<<<<<<< HEAD
+=======
+         <Image 
+         src='/Home/cert.png'
+         alt="Picture of a cute mf"
+         width="500"
+         height="600"
+         />
+
+         <p>#1 best guy</p>
+>>>>>>> 59995d8430b3f753510bfa799ba672b7903c3e37
       </>
    )
 }
