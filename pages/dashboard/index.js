226126429ea48206f563390d248a0c63faf60138
mattchew1010 <mattchewiscool@gmail.com>
2021-12-14 18:36:53 -0500

    updated usercontext

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,11 @@
 import Navbar from '@components/navbar'
 import Metatags from '@components/metatags'
-import {userContext} from '@lib/UserContext'
+import {UserContext} from '@lib/UserContext'
 import {useContext} from 'react';
 
 
 export default function Dashboard() {
-   const { user, username } = useContext(userContext);
+   const { user, username } = useContext(UserContext);
    return (
       <>
          <Metatags title= {"Dashboard  | " + username}/>
