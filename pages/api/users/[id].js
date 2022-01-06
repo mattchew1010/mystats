fddafa603760f01cec413e919d28899523efa8f5
mattchew1010 <mattchewiscool@gmail.com>
2022-01-06 14:39:25 -0500

    basic google auth

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,14 +1,5 @@
-import {firebase} from '../../../lib/firebase'
 
 export default function user(req,res) {
     const id = req.query.id
 
-    const collection = firestore.collection('usernames');
-    const result = usersRef.where('uid', '==', id).limit(1);
-
-    if (result) {
-        res.status(200).json({uid: result})
-    }else {
-        res.status(500).json({ error: 'failed to load data' })
-    }
 }
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,8 @@
 import Navbar from 'components/navbar'
 import Metatags from '@components/metatags'
-import {UserContext} from '@lib/UserContext'
-import {useContext} from 'react';
 
 
 export default function Dashboard() {
-   const { user, username } = useContext(UserContext);
    return (
       <>
          <Metatags title= {"Dashboard  | " + username}/>
