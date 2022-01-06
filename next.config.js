830f26b56a200b8995163d2666e9af7078f05351
mattchew1010 <mattchewiscool@gmail.com>
2022-01-06 12:56:15 -0500

    edit nextconfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const path = require('path')
 module.exports = {
   reactStrictMode: true,
-  webpack: config => {
-    config.resolve.modules.push(path.resolve('./'))
-
-    return config
-  }
+  "baseUrl": "."
 }
 
