a6de3d4626bd0e0d5f0501a50b8a0e1369296b95
mattchew1010 <mattchewiscool@gmail.com>
2022-03-07 18:29:54 -0500

    added session check

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,15 @@
 import Navbar from 'components/navbar'
 import Metatags from '@components/metatags'
-
+import { useSession} from "next-auth/react"
+import { useRouter } from 'next/router'
 
 export default function Dashboard() {
+   const { data: session } = useSession()
+   if (!session) useRouter().push('/login')
+
    return (
       <>
-         <Metatags title= {"Dashboard  | "}/>
+         <Metatags title= {"Dashboard  | " + session.user.name}/>
          <Navbar/>
       </>
    )
