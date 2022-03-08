81756ac5014e2fecb9f900f2ad54d5d3b3c3a519
mattchew1010 <?>
2022-03-08 12:27:12 -0500

    fixed login required page

--- components/loginRequired.js
+++ components/loginRequired.js
@@ -4,10 +4,6 @@ export default function Navbar() {
   return (
      <>
          <h1 style={{textAlign:"center"}}>Not Logged In</h1>
-         <p>Please </p>
-         <Link href="/login">
-            <p>Login</p>
-         </Link>
       </>
    )
 }
