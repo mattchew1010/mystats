5ad8799f3e871aaa3f9712efc9d0536b2b911079
mattchew1010 <?>
2022-03-11 13:12:38 -0500

    NOT WORKING

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,5 +1,15 @@
+import { useRouter } from 'next/router'
+import db from '@lib/db'
 
-export default function user(req,res) {
-    const id = req.query.id
+function Users() {
+    const router = useRouter()
+    const {id} = router.query
+    if (id.match() != id) return (<p>Not Valid UUID</p>)
 
+    let data = {}
+    db.userById(id).then((user) => {
+        data = user
+        console.log(typeof(data))
+    }).catch(console.log)
+    return data
 }
