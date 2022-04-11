b21db96fb56ddfd16471d7f55764c6ebd386013a
mattchew1010 <?>
2022-04-11 12:51:08 -0400

    added username selection page and api

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,11 +1,21 @@
-import {userById} from '@lib/db'
+import {userById,changeUsername} from '@lib/db'
 
 export default function Users(req, res) {
-    console.log(req.query)
-    const id = req.query.id
+const id = req.query.id
+if (req.method === "POST"){
+    if (req.query.setUsername) {
+        if (/^(?=.{8,20}$)(?!.*[_.]{2})[a-zA-Z0-9._]+/.test(req.query.setUsername)) {
+            changeUsername(id,req.query.setUsername).then(res.status(200).json).catch((err) => {
+                res.status(500).send("Internal Error")
+            })
+          }else res.status(400).send("Invalid Username")
+    }
+}
 
+
+if (req.method === "GET"){
     userById(id).then((user) => {
-        console.log(user)
         res.status(200).json(user)
     }).catch()
+}
 }
---
+++ pages/profile/firstlogin/index.js
@@ -0,0 +1,70 @@
+import { useSession} from "next-auth/react"
+import Metatags from '@components/metatags'
+import React from "react"
+import style from '@styles/forms.module.css'
+import {useRouter} from 'next/router'
+
+class UsernameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: '', valid: false};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+      handleChange(event) {
+        console.log(/^(?=.{8,20}$)(?!.*[_.]{2})[a-zA-Z0-9._]+/.test(this.state.value))
+        this.setState({value: event.target.value, valid: /^(?=.{8,20}$)(?!.*[_.]{2})[a-zA-Z0-9._]+/.test(this.state.value)});
+      }
+    
+      handleSubmit(event) {
+        event.preventDefault()
+        const {data} = useSession()
+        const result = fetch('/api/users/' + data.userId + '?setUsername=' + this.state.value)
+        if (result.ok) {
+            useRouter.push('/dashboard')
+        }
+      }
+
+    render() {
+        let message;
+        if (!this.state.valid && this.state.value.length > 0){
+            message = "invalid username"
+        }
+
+        return (
+            <>
+                <div className={style.container}>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        username:
+                        <input className={style.textinput} type="text" value={this.state.value} onChange={this.handleChange} />
+                        <h1 className={style.errorMessage}>
+                            {message}
+                        </h1>
+                    </label>
+                    
+                        <input className={style.submit} type="submit" value="Submit"/>
+                </form>
+            </div>
+        </>
+        )
+    }
+}
+
+
+
+
+
+export default function firstLogon() {
+    const {status} = useSession({required: true})
+    if (status === 'loading') {
+        return <div>Loading...</div>
+    }
+    return (
+        <>
+            <Metatags title="choose a username" description="choose a username"/>
+            <UsernameForm/>
+        </>
+    )
+}
